92b29f5e5cad5fd56eb2970398130a75e2686067
nasimulhasan <aparstechteam@gmail.com>
2025-05-10 11:59:46 +0600

    Refactor submission handling: removed QStash integration, added database queries for submission validation and marks calculation, and updated response status codes.

--- server/api/question/[exam]/index.post.ts
+++ server/api/question/[exam]/index.post.ts
@@ -1,16 +1,6 @@
-import { Client } from "@upstash/qstash";
-
-// Initialize QStash client
-const client = new Client({
-  
-  token: process.env.QSTASH_TOKEN!,
-});
-
 export default defineEventHandler(async (event) => {
   const id = event.context.params?.exam;
   const userId = event.context.user?.id;
-
-  const { answers, submission_id } = await readBody(event);
 
   if (!userId || !id) {
     return createError({
@@ -19,20 +9,73 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  // Publish to QStash
-  await client.publishJSON({
-    url: `${process.env.BASE_URL}/api/queues/process`,
-    body: {
-      submission_id,
-      answers,
-    },
-    retries: 3,
-    notBefore: 0,
-    deduplicationId: submission_id, // Prevent duplicate processing
-  });
+  const { submission_id, answers } = await readBody(event);
 
+  const submission = await query<{
+    id: string;
+    created_at: string;
+  }>(
+    `
+    SELECT * FROM submissions WHERE id = $1
+  `,
+    [submission_id]
+  );
+
+  if (!submission || !submission.data || submission.data.length === 0) {
+    return createError({
+      statusCode: 404,
+      statusMessage: "Submission not found",
+    });
+  }
+
+  let duration =
+    new Date().getTime() - new Date(submission.data[0].created_at).getTime();
+
+  const marks = await query<{
+    marks: number;
+  }>(
+    `
+    SELECT COUNT(*) as marks FROM question_options WHERE id IN (${answers.map((id: string) => `'${id}'`).join(",")}) AND correct = true
+  `
+  );
+
+  if (!marks || !marks.data || marks.data.length === 0) {
+    return createError({
+      statusCode: 404,
+      statusMessage: "Marks not found",
+    });
+  }
+
+  const negMarks = (answers.length - marks?.data[0].marks) * 0.25;
+  const totalMarks = marks?.data[0].marks - negMarks;
+
+  await query(
+    `
+    UPDATE submissions SET
+      answers = $1,
+      duration = $2,
+      submitted_at = $3,
+      marks = $4,
+      correct = $5,
+      incorrect = $6,
+      skipped = $7,
+      status = $8
+    WHERE id = $9
+  `,
+    [
+      answers,
+      duration,
+      new Date(),
+      totalMarks,
+      marks?.data[0].marks,
+      answers.length - marks?.data[0].marks,
+      answers.length - marks?.data[0].marks,
+      "submitted",
+      submission.data[0].id,
+    ]
+  );
   return {
-    statusCode: 202,
-    statusMessage: "Submission queued successfully",
+    statusCode: 200,
+    statusMessage: "Submitted successfully",
   };
 });
